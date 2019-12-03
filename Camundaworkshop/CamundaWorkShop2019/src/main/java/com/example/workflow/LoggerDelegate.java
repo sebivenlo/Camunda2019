f5c40a60000b9ca9c08a3720ea2bee83cc842e50
Ozan Tekin <ozan.tekin@kasse9.de>
2019-12-03 18:21:51 +0100

    logger

---
+++ Camundaworkshop/CamundaWorkShop2019/src/main/java/com/example/workflow/LoggerDelegate.java
@@ -0,0 +1,26 @@
+package com.example.workflow;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import java.util.logging.Logger;
+
+public class LoggerDelegate implements JavaDelegate {
+
+	private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
+	
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+		
+		LOGGER.info("\n\n   First 50% ...  LoggerDelegade invoked by "
+				+ "processDefinitionId=" + execution.getProcessDefinitionId()
+				+ ", activityId=" + execution.getCurrentActivityId()
+				+ ", acitvityName" + execution.getCurrentActivityName()
+				+ ", processInstanceId" + execution.getProcessInstanceId()
+				+ ", business Key " + execution.getProcessBusinessKey()
+				+ "executionId " + execution.getId()
+				+ "\n\n");
+		
+	}
+
+}
